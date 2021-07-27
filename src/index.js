796f61c120975ff68fe2ec95945972165405a9c1
Vls <bestblodd@gmail.com>
2021-07-27 10:25:38 +0300

    add btn/colors/title/center-wrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './assets/css/normalize.scss';
+import './assets/css/vars.scss';
 import './assets/css/main.scss';
+import './assets/css/btn.scss';
 import './assets/css/fonts.scss';
 import './assets/css/blocks/header.scss';
