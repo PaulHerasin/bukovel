47d85532e6cc7e45192a82d7ef3c46c0db0d0b9b
Vls <bestblodd@gmail.com>
2021-07-27 11:04:25 +0300

    add general style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// CSS
 import './assets/css/normalize.scss';
 import './assets/css/vars.scss';
 import './assets/css/main.scss';
 import './assets/css/btn.scss';
 import './assets/css/fonts.scss';
 import './assets/css/blocks/header.scss';
+
+
+// JS
+import './assets/js/header';
