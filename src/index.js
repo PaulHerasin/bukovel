de12e6a8765104262f95b0574f6d8ec71e4ded1e
pavel <geraci98.98@gmail.com>
2021-07-27 11:05:46 +0300

    add footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './assets/css/normalize.scss';
 import './assets/css/main.scss';
 import './assets/css/fonts.scss';
+
 import './assets/css/blocks/header.scss';
+import './assets/css/blocks/footer.scss';
